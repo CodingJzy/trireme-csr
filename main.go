2551ec87bbf6cc1b0bff18d959100486cee9c614
Bernard Van De Walle <bernard@bvandewa.com>
2017-09-28 17:19:28 -0700

    App skeleton

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	setLogs("info")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	zap.L().Info("Everything started. Waiting for Stop signal")
+	// Waiting for a Sig
+	<-c
+	zap.L().Info("SIG received. Exiting")
+}
+
+// setLogs setups Zap to
+func setLogs(logLevel string) error {
+	zapConfig := zap.NewDevelopmentConfig()
+	zapConfig.DisableStacktrace = true
+
+	// Set the logger
+	switch logLevel {
+	case "trace":
+		zapConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	case "debug":
+		zapConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	case "info":
+		zapConfig.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	case "warn":
+		zapConfig.Level = zap.NewAtomicLevelAt(zap.WarnLevel)
+	case "error":
+		zapConfig.Level = zap.NewAtomicLevelAt(zap.ErrorLevel)
+	case "fatal":
+		zapConfig.Level = zap.NewAtomicLevelAt(zap.FatalLevel)
+	default:
+		zapConfig.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	}
+
+	logger, err := zapConfig.Build()
+	if err != nil {
+		return err
+	}
+
+	zap.ReplaceGlobals(logger)
+	return nil
+}
