ef4b1124c982fba74c50f27ef4018c3851ed515e
Bernard Van De Walle <bernard@bvandewa.com>
2017-10-05 13:58:21 -0700

    Adding dockerfile definition

---
+++ version/version.go
@@ -0,0 +1,7 @@
+package version
+
+// VERSION is the version of Trireme-Kubernetes
+const VERSION = "0.11"
+
+// REVISION is the revision of Trireme-Kubernetes
+const REVISION = "7c7a3816675e0b6e88bd52d627869945a68452e0"
