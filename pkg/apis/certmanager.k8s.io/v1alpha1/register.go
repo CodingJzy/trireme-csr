9e3412d487753222163b356517978dc8e1cc3846
Marcus Heese <marcus.heese@gmail.com>
2018-02-19 12:18:30 -0800

    recreating API files in pkg/apis for code generator

---
+++ pkg/apis/certmanager.k8s.io/v1alpha1/register.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	certmanagerk8sio "github.com/aporeto-inc/trireme-csr/pkg/apis/certmanager.k8s.io"
+)
+
+// SchemeGroupVersion is the group version used to register these objects.
+var SchemeGroupVersion = schema.GroupVersion{Group: certmanagerk8sio.GroupName, Version: "v1alpha1"}
+
+var (
+	// SchemeBuilder defines the scheme builder for this type
+	SchemeBuilder      = runtime.NewSchemeBuilder(addKnownTypes)
+	localSchemeBuilder = &SchemeBuilder
+	// AddToScheme shortcut definition
+	AddToScheme = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+// Kind takes an unqualified kind and returns back a Group qualified GroupKind
+func Kind(kind string) schema.GroupKind {
+	return SchemeGroupVersion.WithKind(kind).GroupKind()
+}
+
+// Resource takes an unqualified resource and returns a Group-qualified GroupResource.
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// addKnownTypes adds the set of types defined in this package to the supplied scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Certificate{},
+		&CertificateList{},
+	)
+
+	scheme.AddKnownTypes(SchemeGroupVersion, &metav1.Status{})
+
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
---
+++ pkg/apis/certmanager.k8s.io/v1alpha1/types.go
@@ -0,0 +1,53 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// CertificateResourcePlural is the ressource name used to get a list of cetts.
+const CertificateResourcePlural = "certificates"
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Certificate is the specification for the Certificate object on the Kubernetes API
+type Certificate struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              CertificateSpec   `json:"spec"`
+	Status            CertificateStatus `json:"status,omitempty"`
+}
+
+// CertificateSpec is the specification for Certificates on the API
+type CertificateSpec struct {
+	// Base64-encoded PKCS#10 CSR data
+	Request []byte `json:"request" protobuf:"bytes,1,opt,name=request"`
+}
+
+// CertificateStatus is the status for Certificates on the API
+type CertificateStatus struct {
+	State       CertificateState `json:"state,omitempty"`
+	Certificate []byte           `json:"certificate,omitempty" protobuf:"bytes,2,opt,name=certificate"`
+	Token       []byte           `json:"token,omitempty" protobuf:"bytes,2,opt,name=token"`
+	Ca          []byte           `json:"ca,omitempty" protobuf:"bytes,2,opt,name=ca"`
+}
+
+// CertificateState defines the state of the certificate
+type CertificateState string
+
+const (
+	// CertificateStateCreated defines that the Certificate was created
+	CertificateStateCreated CertificateState = "Created"
+	// CertificateStateProcessed defines that the certificate was processed
+	CertificateStateProcessed CertificateState = "Processed"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CertificateList represents a list of certificate
+type CertificateList struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ListMeta `json:"metadata"`
+	Items           []Certificate `json:"items"`
+}
